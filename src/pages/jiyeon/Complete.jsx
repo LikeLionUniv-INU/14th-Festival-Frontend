e16ae747a5aec040c2c83bf9a61986966ecd741e
jiyoeo <jiyoeo2@gmail.com>
2026-04-09 22:49:05 +0900

    Feat: 버튼 컴포넌트 제작 및 완료 페이지 제작

--- src/pages/jiyeon/Complete.jsx
+++ src/pages/jiyeon/Complete.jsx
@@ -1,6 +1,17 @@
-function Complete(){
+import React from 'react';
+import Button from "../../components/Button.jsx";
+import * as S from "./styles/complete.styles.js";
+
+
+function Complete({ title, fontSize, gap = "10px"}){
     return(
-        <h2>안녕</h2>
+        <S.Container>
+            <S.Title $fontSize={fontSize}>{title}</S.Title>
+            <S.Lion $gap={gap}>
+                <img src = "/assets/smileLion.png"/>
+            </S.Lion>
+            <Button>다음</Button>
+        </S.Container>
     );
 }
 
--- src/pages/jiyeon/JiyeonRouter.js
+++ src/pages/jiyeon/JiyeonRouter.js
@@ -7,7 +7,19 @@ import Complete from "./Complete";
 export default function JiyeonRouter() {
   return (
     <Routes>
-      <Route path="/complete" element={<Complete />} />
+      <Route path="/complete" element={<Complete 
+      title="선택 완료!"
+      gap="35px"/>} />
+
+      <Route path= "/about-me" element={<Complete title="질문에 대한
+답변을 선택해줘!"
+      fontSize="2rem" />} />
+
+      <Route path= "/about-you" element={<Complete title="이제 내가 원하는
+상대를 골라보자!"
+      fontSize="2rem" />} />
+
+      {/* 순서대로 선택 완료, 본인 선택, 상대 선택 페이지 */}
       {/* <Route path="login" element={<Login />} /> 이런 식으로 본인이 만든 페이지 라우팅 설정하세용*/}
       {/* 자기가 만든 페이지 확인하고 싶으면 이거 연동 다 하고 주소 뒤에 jiyeon/login 처럼 입력하면 됨 */}
     </Routes>

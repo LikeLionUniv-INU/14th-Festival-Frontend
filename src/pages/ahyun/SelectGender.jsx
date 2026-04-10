f2ef00be08735798ab4c7dfa3582e98002b7a227
unknown <kah070809@gmail.com>
2026-04-11 03:41:06 +0900

    Feat: 성별 페이지

--- src/pages/ahyun/SelectGender.jsx
+++ src/pages/ahyun/SelectGender.jsx
@@ -3,7 +3,8 @@ import {useNavigate} from 'react-router-dom'
 import *as S from './style/gender.style'; 
 
 // 공통 버튼 불러오기 
-import Button from '../../components/Button';
+import Button from '../../components/common/Button';
+import MessageCard from '../../components/common/MessageCard';
 
 const Gender = () => {
     const [selectedGender, setSelectedGender] = useState(null);
@@ -13,10 +14,7 @@ const Gender = () => {
 return(
     <S.Container>
         <S.Guide>
-            <img src="14th-Festival-Frontend\public\assets\basicLion.png" />
-            <S.SBubble>
-                <p>성별을 선택해 주세요</p>
-            </S.SBubble>
+            <MessageCard imageUrl="/assets/smallbasicLion.png" text="성별을 선택해 주세요" />
         </S.Guide>
         <S.BSection>
             <S.GenderButton
--- src/pages/ahyun/style/gender.style.js
+++ src/pages/ahyun/style/gender.style.js
@@ -3,60 +3,46 @@ import styled from 'styled-components';
 export const NButton = styled.div`
     display: flex;
     justify-content: center;
-    
     width: 100%;
+    margin-bottom: 20px;
 `;
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-
     width: 100%;
-    height: 100vh;
-    justify-content: space-between;
-    padding-top: 100px;
-    background-color: #FFFFFF;
-    height: 100dvh;
+    
+    justify-content: flex-start;
+    gap: 50px;
+    padding: 60px 0;
+    background-color:transparent;
+    box-sizing: border-box;
 `;
 
 export const Guide = styled.div`
     display: flex;
-    flex-direction: row;
-    align-items: center;
-    gap: 15px;  // 사진이랑 말풍선 사이 간격 //
-    margin-bottom: 50px;
-    width: 90%;
-    text-align: center;
+    justify-content: center;
+    width: 100%;
 `; 
 
-export const SBubble = styled.div`
-    flex: 1;
-    padding: 15px 20px;
-    background-color: #FFFFFF;
-    border: 1px solid #AA6734;
-    border-radius: 15px;
-    font-size: 16px;
-    color: #000000;
-`;
 
 export const BSection = styled.div`
     display: flex;
     gap: 20px;
-    margin-bottom: 100px;
+    justify-content: center;
 `;
 
 export const GenderButton = styled.button`
     width: 166px;
     height: 210px;
-    border-radius: 20px;
+    border-radius: 26px;
     font-size: 28px;
     font-weight: bold;
-    border: 1px solid #F0C5CA;
+    border: 1px solid rgba(240, 197, 202, 1);
     transition: all 0.2s;
     //선택 여부에 따라 색 바꿈
-    background-color: ${(props) => (props.isSelected ? 'rgba(230, 158, 166, 0.74)' : '#FFFFFF')};
-    color: ${(props) => (props.isSelected ? '#FFFFFF' : '#EB9850')};
+    background-color: ${(props) => (props.isSelected ? 'rgba(230, 158, 166, 1)' : '#FFFFFF')};
+    color: ${(props) => (props.isSelected ? '#FFFFFF' : 'rgba(161, 92, 41, 1)')};
 `;
-
 

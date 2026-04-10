995d54c78638197ae0f8e98cf9c3e184ae05ca6a
unknown <namyoon0418@gmail.com>
2026-04-11 04:35:53 +0900

    시작단계

---
+++ src/pages/namyoon/Alcohol.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import TagBlock, { TagGrid } from './Components';
+
+import Button from "../../components/common/Button";
+
+import MessageCard from "../../components/common/MessageCard";

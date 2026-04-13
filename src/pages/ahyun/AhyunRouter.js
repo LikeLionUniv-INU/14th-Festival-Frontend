97dc5d982901d79aa9ba7635364db6453d773a6a
kah070809-rgb <kah070809@gmail.com>
2026-04-13 13:28:08 +0900

    Feat : 성별 선택 및 로그인 페이지 디자인  (#21)
    * Feat : 성별 선택 및 로그인 페이지 디자인

--- src/pages/ahyun/AhyunRouter.js
+++ src/pages/ahyun/AhyunRouter.js
@@ -4,13 +4,14 @@ import { Routes, Route } from "react-router-dom";
 // import Login from "./Login"; 이런 식으로 본인이 만든 페이지 불러와서
 import Gender from './SelectGender';
 import MessageCard from "../../components/common/MessageCard";
+import Login from "./Login";
 
 export default function AhyunRouter() {
   return (
     <Routes>
-        <Route element={<MessageCard />}/>
-          <Route path="gender" element={<Gender/>}/>
-
+        <Route path="message" element={<MessageCard />}/>
+        <Route path="gender" element={<Gender/>}/>
+        <Route path="login" element={<Login />} />
         {/* <Route path="login" element={<Login />} /> 이런 식으로 본인이 만든 페이지 라우팅 설정하세용*/}
         {/* 자기가 만든 페이지 확인하고 싶으면 이거 연동 다 하고 주소 뒤에 ahyun/login 처럼 입력하면 됨 */}
     </Routes>

f13c7079325f8b4d8892be3a6457084e27e3a993
Lim Sanghyun <sh2624@daum.net>
2026-04-14 18:22:53 +0900

    Fix: 선택 시 리렌더링 오류 해결 (#28)
    * Fix: 선택 시 리렌더링 오류 해결

--- src/pages/Login.styles.js
+++ src/pages/Login.styles.js
@@ -1,90 +1,86 @@
 // 2-1. 정보 입력 → 개인정보 수집 동의 (아현)
 
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center; 
-    
-    width: 100%;
-    height: 100vh; 
-    position: fixed; 
-    top: 0;
-    left: 0;
-    
-    background-color: transparent;
-    box-sizing: border-box;
-    overflow: hidden;
-    `;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
-    export const Content = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: rgba(255, 255, 255, 1);
-    border-radius: 20px;
-    padding: 40px 25px 25px 25px ;
-    border: 1px solid rgba(171, 108, 56, 1);
-    box-sizing: border-box;
-    width: 311px;
-    height: 521.16px;
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  top: 0;
+  left: 0;
 
-    margin-bottom: 20px; 
-    position: relative;
-    z-index: 1;
-    `;
+  background-color: transparent;
+  box-sizing: border-box;
+  overflow: hidden;
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 20px;
+  padding: 40px 25px 25px 25px;
+  border: 1px solid rgba(171, 108, 56, 1);
+  box-sizing: border-box;
+  width: 311px;
+  height: 521.16px;
+
+  margin-bottom: 20px;
+  position: relative;
+  z-index: 1;
+`;
 
 export const Img = styled.img`
-    width: 116px;
-    height: 116px;
-    margin-bottom: 20px;
-    `;
+  width: 116px;
+  height: 116px;
+  margin-bottom: 20px;
+`;
 
 export const LoginTitle = styled.div`
-    font-size: 20px;
-    font-weight: 700;
-    white-space: pre-line;
-    line-height: 1.5;
-    color: rgba(0, 0, 0, 1);
-    margin-bottom: 30px;
-    font-family: 'NEXON Lv1 Gothic ', sans-serif;
-    text-align: center;
-    `;
+  font-size: 20px;
+  font-weight: 700;
+  white-space: pre-line;
+  line-height: 1.5;
+  color: rgba(0, 0, 0, 1);
+  margin-bottom: 30px;
+  text-align: center;
+`;
 
 export const InputBox = styled.input`
-    width: 204.55px;
-    height: 34px;
-    background: rgba(255, 255, 255, 1);
-    border: 1px solid rgba(0, 0, 0, 1);
-    border-radius: 8px;
-    padding:10px;
-    box-sizing: border-box;
-    outline: none;
+  width: 204.55px;
+  height: 34px;
+  background: rgba(255, 255, 255, 1);
+  border: 1px solid rgba(0, 0, 0, 1);
+  border-radius: 8px;
+  padding: 10px;
+  box-sizing: border-box;
+  outline: none;
 
-    //사용자가 입력할 글자 스타일
-    font-family: 'NEXON Lv1 Gothic', sans-serif;
+  //사용자가 입력할 글자 스타일
+  font-family: "NEXON Lv1 Gothic", sans-serif;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 1);
+
+  &::placeholder {
+    font-family: "NEXON Lv1 Gothic", sans-serif;
     font-size: 14px;
-    color: rgba(0, 0, 0, 1);
-    
-    &::placeholder {
-        font-family: 'NEXON Lv1 Gothic', sans-serif;
-        font-size: 14px;
-        color: rgba(166, 166, 166, 1);
-        }
+    color: rgba(166, 166, 166, 1);
+  }
 `;
 
 export const GuideText = styled.div`
-    font-family: 'NEXON Lv1 Gothic', sans-serif;
-    font-size: 10px;
-    line-height: 1.0;
-    font-weight: 300;
-    color: rgba(0, 0, 0, 1);
-    margin: 6px 0 22px 0;
-    text-align: left;
-    width: 207px;
+  font-family: "NEXON Lv1 Gothic", sans-serif;
+  font-size: 10px;
+  line-height: 1;
+  font-weight: 300;
+  color: rgba(0, 0, 0, 1);
+  margin: 6px 0 22px 0;
+  text-align: left;
+  width: 207px;
 `;
-
-
-
--- src/styles/GlobalStyle.jsx
+++ src/styles/GlobalStyle.jsx
@@ -1,117 +1,62 @@
-// 페이지 전체에 적용되는 전역 스타일
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
-  @font-face {
-    font-family: 'SUIT';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Regular.woff2') format('woff2');
-    font-weight: 400;
-    font-style: normal;
-  }
-  
-  @font-face {
-    font-family: 'SUIT';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Medium.woff2') format('woff2');
-    font-weight: 500;
-    font-style: normal;
-  }
-  
-  @font-face {
-    font-family: 'SUIT';
-    src: url('https://fastly.jsdelivr.net/gh/projectnoonnu/noonfonts_suit@1.0/SUIT-Bold.woff2') format('woff2');
-    font-weight: 700;
-    font-style: normal;
-  }
-  
-// 버튼 폰트
-  @font-face {
-    font-family:'NexonLv1Gothic';
-    src: url('../fonts/NEXONLV1GothicBold.ttf') format('truetype');
-    font-weight: 700;
-    font-display: swap;
-  }
-
-  @font-face {
-    font-family:'NexonLv1Gothic';
-    src: url('../fonts/NEXONLV1GothicRegular.ttf') format('truetype');
-    font-weight: 500;
-    font-display: swap;
-  }
-  
-  @font-face {
-    font-family: 'NexonLv1Gothic';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_20-04@2.1/NEXON Lv1 Gothic OTF Light.woff') format('woff');
-    font-weight: 300;
-    font-display: swap;
-}
-
-
-// 본문 폰트
-@font-face {
-    font-family: 'YeogiOttaeJalnan';
-    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_four@1.2/JalnanOTF00.woff') format('woff');
-    font-weight: normal;
-    font-display: swap;
-}
-
-  /* 실제 화면 높이 변수 */
   :root {
     --vh: 1vh;
     --app-height: 100vh;
+    --font-main: 'NexonLv1Gothic', sans-serif;
+    --font-point: 'YeogiOttaeJalnan', sans-serif;
   }
 
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+    font-family: var(--font-main); 
   }
 
   html, body {
-    /* 스크롤 방지 */
     overflow: hidden;
     position: fixed;
     width: 100%;
-    height: var(--app-height, 90vh);
-    /* IOS 모멘텀 스크롤 방지 */
+    height: var(--app-height);
+    background-color: #ffffff;
     -webkit-overflow-scrolling: touch;
     overscroll-behavior: none;
     touch-action: none;
   }
 
   #root {
-    height: var(--app-height, 90vh);
-    max-height: var(--app-height, 90vh);
+    width: 100%;
+    height: var(--app-height);
     overflow: hidden;
-    position: relative;
   }
 
   body {
-    font-family: 'YeogiOttaeJalnan', sans-serif !important;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    background-color:rgb(255, 255, 255);
-    color: #333;  
-    /* 터치 하이라이트 방지 */
+    font-family: var(--font-main) !important;
+    color: #333;
     -webkit-tap-highlight-color: transparent;
+  }
+
+  h1, h2, .point-text {
+    font-family: var(--font-point);
+  }
+
+  button, p, h3, span, input, textarea {
+    font-family: var(--font-main);
+    color: #000000;
+  }
+
+  button {
+    cursor: pointer;
+    background: none;
+    border: none;
   }
 
   a {
     text-decoration: none;
     color: inherit;
   }
-
-  input, textarea, select {
-    font-family: inherit; 
-  }
-
- button, p, h3 {
-  color: #000000;
-  background: none;
-  border: none;
-
-  font-family: 'NexonLv1Gothic', sans-serif;
-  cursor: pointer;
-}
 `;
 
 export default GlobalStyle;

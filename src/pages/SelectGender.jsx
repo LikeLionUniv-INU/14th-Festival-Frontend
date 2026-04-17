aca0ad40595b68ab04af47f9497a144feb765f69
namyoon0418 <namyoon0418@gmail.com>
2026-04-17 17:03:27 +0900

    Style: 이미지 형식 변환 (#40)
    * png를 webp로 변환

--- src/pages/SelectGender.jsx
+++ src/pages/SelectGender.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./SelectGender.styles";
-import smallBasicLion from "../assets/images/lion/small-basic-lion.png";
+import smallBasicLion from "../assets/images/lion/small-basic-lion.webp";
 import ProgressBar from "../components/common/ProgressBar";
 // 공통 버튼 불러오기
 import Button from "../components/common/Button";

0f00ea8b8819a847601acb481047e785b9604b80
김남윤 <namyoon0418@gmail.com>
2026-04-26 11:54:16 +0900

    임시저장이요

--- src/pages/ResultCheckPage.jsx
+++ src/pages/ResultCheckPage.jsx
@@ -22,7 +22,16 @@ const ResultCheckPage = () => {
       });
 
       if (response.data.isSuccess) {
-        navigate("/match-success");
+        const isMatched = response.data.result?.isMatched;
+
+        if (isMatched) {
+          const { partnerInstagramId } = response.data.result;
+
+          navigate("/match-success", { state: { partnerId: partnerInstagramId } });
+        }
+        else {
+          navigate("/match-fail");
+        }
       }
     }
     catch (error) {
